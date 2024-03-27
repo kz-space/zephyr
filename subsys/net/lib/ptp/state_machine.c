4c1b72f35ed20da07f9b34e24eeb8394b977df65
Adam Wojasinski <awojasinski@baylibre.com>
2024-03-27 16:06:47 +0100

    net: ptp: Add finite state machine implementation for PTP stack
    The patch brings implementationof a state machine for full
    PTP implementation. It returns new PTP Port's state based
    on event that occured and previous state.
    
    Based on the figure 30 in section 9.2.5 of the IEEE 1588-2019.
    
    Signed-off-by: Adam Wojasinski <awojasinski@baylibre.com>

---
+++ subsys/net/lib/ptp/state_machine.c
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2024 BayLibre SAS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+#include "state_machine.h"
+
+enum ptp_port_state ptp_state_machine(enum ptp_port_state state,
+				      enum ptp_port_event event,
+				      bool master_diff)
+{
+	enum ptp_port_state new_state = state;
+
+	if (event == PTP_EVT_INITIALIZE || event == PTP_EVT_POWERUP) {
+		/* initialize port data sets, HW and communication facilities */
+		return PTP_PS_INITIALIZING;
+	}
+
+	switch (state) {
+	case PTP_PS_INITIALIZING:
+		switch (event) {
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = PTP_PS_FAULTY;
+			break;
+		case PTP_EVT_INIT_COMPLETE:
+			new_state = PTP_PS_LISTENING;
+			break;
+		default:
+			break;
+		}
+		break;
+	case PTP_PS_FAULTY:
+		switch (event) {
+		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = PTP_PS_DISABLED;
+			break;
+		case PTP_EVT_FAULT_CLEARED:
+			new_state = PTP_PS_INITIALIZING;
+			break;
+		default:
+			break;
+		}
+		break;
+	case PTP_PS_DISABLED:
+		if (event == PTP_EVT_DESIGNATED_ENABLED) {
+			new_state = PTP_PS_INITIALIZING;
+		}
+		break;
+	case PTP_PS_LISTENING:
+		switch (event) {
+		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = PTP_PS_DISABLED;
+			break;
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = PTP_PS_FAULTY;
+			break;
+		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
+			new_state = PTP_PS_MASTER;
+			break;
+		case PTP_EVT_RS_MASTER:
+			new_state = PTP_PS_PRE_MASTER;
+			break;
+		case PTP_EVT_RS_GRAND_MASTER:
+			new_state = PTP_PS_GRAND_MASTER;
+			break;
+		case PTP_EVT_RS_PASSIVE:
+			new_state = PTP_PS_PASSIVE;
+			break;
+		case PTP_EVT_RS_SLAVE:
+			new_state = PTP_PS_UNCALIBRATED;
+			break;
+		default:
+			break;
+		}
+		break;
+	case PTP_PS_PRE_MASTER:
+		switch (event) {
+		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = PTP_PS_DISABLED;
+			break;
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = PTP_PS_FAULTY;
+			break;
+		case PTP_EVT_QUALIFICATION_TIMEOUT_EXPIRES:
+			new_state = PTP_PS_MASTER;
+			break;
+		case PTP_EVT_RS_PASSIVE:
+			new_state = PTP_PS_PASSIVE;
+			break;
+		case PTP_EVT_RS_SLAVE:
+			new_state = PTP_PS_UNCALIBRATED;
+			break;
+		default:
+			break;
+		}
+		break;
+	case PTP_PS_MASTER:
+		switch (event) {
+		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = PTP_PS_DISABLED;
+			break;
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = PTP_PS_FAULTY;
+			break;
+		case PTP_EVT_RS_PASSIVE:
+			new_state = PTP_PS_PASSIVE;
+			break;
+		case PTP_EVT_RS_SLAVE:
+			new_state = PTP_PS_UNCALIBRATED;
+			break;
+		default:
+			break;
+		}
+		break;
+	case PTP_PS_PASSIVE:
+		switch (event) {
+		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = PTP_PS_DISABLED;
+			break;
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = PTP_PS_FAULTY;
+			break;
+		case PTP_EVT_RS_MASTER:
+			new_state = PTP_PS_PRE_MASTER;
+			break;
+		case PTP_EVT_RS_GRAND_MASTER:
+			new_state = PTP_PS_GRAND_MASTER;
+			break;
+		case PTP_EVT_RS_SLAVE:
+			new_state = PTP_PS_UNCALIBRATED;
+			break;
+		default:
+			break;
+		}
+		break;
+	case PTP_PS_UNCALIBRATED:
+		switch (event) {
+		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = PTP_PS_DISABLED;
+			break;
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = PTP_PS_FAULTY;
+			break;
+		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
+			new_state = PTP_PS_MASTER;
+			break;
+		case PTP_EVT_MASTER_CLOCK_SELECTED:
+			new_state = PTP_PS_SLAVE;
+			break;
+		case PTP_EVT_RS_MASTER:
+			new_state = PTP_PS_PRE_MASTER;
+			break;
+		case PTP_EVT_RS_GRAND_MASTER:
+			new_state = PTP_PS_GRAND_MASTER;
+			break;
+		case PTP_EVT_RS_PASSIVE:
+			new_state = PTP_PS_PASSIVE;
+			break;
+		default:
+			break;
+		}
+		break;
+	case PTP_PS_SLAVE:
+		switch (event) {
+		case PTP_EVT_DESIGNATED_DISABLED:
+			new_state = PTP_PS_DISABLED;
+			break;
+		case PTP_EVT_FAULT_DETECTED:
+			new_state = PTP_PS_FAULTY;
+			break;
+		case PTP_EVT_SYNCHRONIZATION_FAULT:
+			new_state = PTP_PS_UNCALIBRATED;
+			break;
+		case PTP_EVT_ANNOUNCE_RECEIPT_TIMEOUT_EXPIRES:
+			new_state = PTP_PS_MASTER;
+			break;
+		case PTP_EVT_RS_MASTER:
+			new_state = PTP_PS_PRE_MASTER;
+			break;
+		case PTP_EVT_RS_GRAND_MASTER:
+			new_state = PTP_PS_GRAND_MASTER;
+			break;
+		case PTP_EVT_RS_PASSIVE:
+			new_state = PTP_PS_PASSIVE;
+			break;
+		case PTP_EVT_RS_SLAVE:
+			if (master_diff) {
+				new_state = PTP_PS_UNCALIBRATED;
+			}
+			break;
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return new_state;
+}
