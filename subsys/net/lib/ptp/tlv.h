b3f59ddb4d7aefcf2498a25bfbfa4a546a59c89c
Adam Wojasinski <awojasinski@baylibre.com>
2024-05-20 12:00:38 +0200

    net: ptp: Extend TLV with Management TLVs
    This commit adds handling of Management TLVs
    
    Signed-off-by: Adam Wojasinski <awojasinski@baylibre.com>

--- subsys/net/lib/ptp/tlv.h
+++ subsys/net/lib/ptp/tlv.h
@@ -20,6 +20,9 @@ extern "C" {
 
 #include <stdint.h>
 
+#include "ddt.h"
+#include "msg.h"
+
 /**
  * @brief Type of TLV (type, length, value)
  *
@@ -49,6 +52,94 @@ enum ptp_tlv_type {
 };
 
 /**
+ * @brief PTP managenment message action field
+ *
+ * @note based on IEEE 1588-2019 Section 15.4.1.6 Table 57
+ */
+enum ptp_mgmt_op {
+	PTP_MGMT_GET,
+	PTP_MGMT_SET,
+	PTP_MGMT_RESP,
+	PTP_MGMT_CMD,
+	PTP_MGMT_ACK,
+};
+
+/**
+ * @brief PTP managenment message ID
+ *
+ * @note based on IEEE 1588-2019 Section 15.5.2.3 Table 59
+ */
+enum ptp_mgmt_id {
+	PTP_MGMT_NULL_PTP_MANAGEMENT = 0x0,
+	PTP_MGMT_CLOCK_DESCRIPTION,
+	PTP_MGMT_USER_DESCRIPTION,
+	PTP_MGMT_SAVE_IN_NON_VOLATILE_STORAGE,
+	PTP_MGMT_RESET_NON_VOLATILE_STORAGE,
+	PTP_MGMT_INITIALIZE,
+	PTP_MGMT_FAULT_LOG,
+	PTP_MGMT_FAULT_LOG_RESET,
+	PTP_MGMT_DEFAULT_DATA_SET = 0x2000,
+	PTP_MGMT_CURRENT_DATA_SET,
+	PTP_MGMT_PARENT_DATA_SET,
+	PTP_MGMT_TIME_PROPERTIES_DATA_SET,
+	PTP_MGMT_PORT_DATA_SET,
+	PTP_MGMT_PRIORITY1,
+	PTP_MGMT_PRIORITY2,
+	PTP_MGMT_DOMAIN,
+	PTP_MGMT_SLAVE_ONLY,
+	PTP_MGMT_LOG_ANNOUNCE_INTERVAL,
+	PTP_MGMT_ANNOUNCE_RECEIPT_TIMEOUT,
+	PTP_MGMT_LOG_SYNC_INTERVAL,
+	PTP_MGMT_VERSION_NUMBER,
+	PTP_MGMT_ENABLE_PORT,
+	PTP_MGMT_DISABLE_PORT,
+	PTP_MGMT_TIME,
+	PTP_MGMT_CLOCK_ACCURACY,
+	PTP_MGMT_UTC_PROPERTIES,
+	PTP_MGMT_TRACEBILITY_PROPERTIES,
+	PTP_MGMT_TIMESCALE_PROPERTIES,
+	PTP_MGMT_UNICAST_NEGOTIATION_ENABLE,
+	PTP_MGMT_PATH_TRACE_LIST,
+	PTP_MGMT_PATH_TRACE_ENABLE,
+	PTP_MGMT_GRANDMASTER_CLUSTER_TABLE,
+	PTP_MGMT_UNICAST_MASTER_TABLE,
+	PTP_MGMT_UNICAST_MASTER_MAX_TABLE_SIZE,
+	PTP_MGMT_ACCEPTABLE_MASTER_TABLE,
+	PTP_MGMT_ACCEPTABLE_MASTER_TABLE_ENABLED,
+	PTP_MGMT_ACCEPTABLE_MASTER_MAX_TABLE_SIZE,
+	PTP_MGMT_ALTERNATE_MASTER,
+	PTP_MGMT_ALTERNATE_TIME_OFFSET_ENABLE,
+	PTP_MGMT_ALTERNATE_TIME_OFFSET_NAME,
+	PTP_MGMT_ALTERNATE_TIME_OFFSET_MAX_KEY,
+	PTP_MGMT_ALTERNATE_TIME_OFFSET_PROPERTIES,
+	PTP_MGMT_EXTERNAL_PORT_CONFIGURATION_ENABLED = 0x3000,
+	PTP_MGMT_MASTER_ONLY,
+	PTP_MGMT_HOLDOVER_UPGRADE_ENABLE,
+	PTP_MGMT_EXT_PORT_CONFIG_PORT_DATA_SET,
+	PTP_MGMT_TRANSPARENT_CLOCK_DEFAULT_DATA_SET = 0x4000,
+	PTP_MGMT_TRANSPARENT_CLOCK_PORT_DATA_SET,
+	PTP_MGMT_PRIMARY_DOMAIN,
+	PTP_MGMT_DELAY_MECHANISM = 0x6000,
+	PTP_MGMT_LOG_MIN_PDELAY_REQ_INTERVAL,
+};
+
+/**
+ * @brief Management error ID
+ *
+ * @note based on IEEE 1588-2019 Section 15.5.4.4 Table 109
+ */
+enum ptp_mgmt_err {
+	PTP_MGMT_ERR_RESPONSE_TOO_BIG = 0x1,
+	PTP_MGMT_ERR_NO_SUCH_ID,
+	PTP_MGMT_ERR_WRONG_LENGTH,
+	PTP_MGMT_ERR_WRONG_VALUE,
+	PTP_MGMT_ERR_NOT_SETABLE,
+	PTP_MGMT_ERR_NOT_SUPPORTED,
+	PTP_MGMT_ERR_UNPOPULATED,
+	PTP_MGMT_ERR_GENERAL = 0xFFFE,
+};
+
+/**
  * @brief PAD TLV - used to increase length of any PTP message.
  *
  * @note 14.4.2 - PAD TLV
@@ -72,10 +163,120 @@ struct ptp_tlv_org {
 	uint8_t  data[];
 } __packed;
 
-struct ptp_tlv_container {
-	sys_snode_t    node;
-	struct ptp_tlv *tlv;
+/**
+ * @brief Management TLV.
+ *
+ * @note 15.5.2 - MANAGEMENT TLV field format.
+ */
+struct ptp_tlv_mgmt {
+	uint16_t type;
+	uint16_t length;
+	uint16_t id;
+	uint8_t  data[];
+} __packed;
+
+/**
+ * @brief Management error status TLV.
+ *
+ * @note 15.5.4 - MANAGEMENT_ERROR_STATUS TLV format.
+ */
+struct ptp_tlv_mgmt_err {
+	uint16_t	type;
+	uint16_t	length;
+	uint16_t	err_id;
+	uint16_t	id;
+	uint32_t	reserved;
+	struct ptp_text display_data;
+} __packed;
+
+/**
+ * @brief Structure holding pointers for Clock description send over TLV
+ */
+struct ptp_tlv_mgmt_clock_desc {
+	uint16_t	     *type;
+	struct ptp_text	     *phy_protocol;
+	uint16_t	     *phy_addr_len;
+	uint8_t		     *phy_addr;
+	struct ptp_port_addr *protocol_addr;
+	uint8_t		     *manufacturer_id;
+	struct ptp_text	     *product_desc;
+	struct ptp_text	     *revision_data;
+	struct ptp_text	     *user_desc;
+	uint8_t		     *profile_id;
 };
+
+/**
+ * @brief Structure holding TLV. It is used as a helper to retrieve TLVs from PTP messages.
+ */
+struct ptp_tlv_container {
+	sys_snode_t		       node;
+	struct ptp_tlv		       *tlv;
+	struct ptp_tlv_mgmt_clock_desc clock_desc;
+};
+
+/**
+ * @brief TLV data fields representing defaultDS dataset.
+ */
+struct ptp_tlv_default_ds {
+	uint8_t		       flags;
+	uint8_t		       reserved1;
+	uint16_t	       n_ports;
+	uint8_t		       priority1;
+	struct ptp_clk_quality clk_quality;
+	uint8_t		       priority2;
+	ptp_clk_id	       clk_id;
+	uint8_t		       domain;
+	uint8_t		       reserved2;
+} __packed;
+
+/**
+ * @bref TLV data fields representing currentDS dataset.
+ */
+struct ptp_tlv_current_ds {
+	uint16_t	 steps_rm;
+	ptp_timeinterval offset_from_master;
+	ptp_timeinterval mean_delay;
+} __packed;
+
+/**
+ * @brief TLV data fields representing parentDS dataset.
+ */
+struct ptp_tlv_parent_ds {
+	struct ptp_port_id     port_id;
+	uint8_t		       flags;
+	uint8_t		       reserved;
+	uint16_t	       obsreved_parent_offset_scaled_log_variance;
+	int32_t		       obsreved_parent_clk_phase_change_rate;
+	uint8_t		       gm_priority1;
+	struct ptp_clk_quality gm_clk_quality;
+	uint8_t		       gm_priority2;
+	ptp_clk_id	       gm_id;
+} __packed;
+
+/**
+ * @brief TLV data fields representing time_propertiesDS dataset.
+ */
+struct ptp_tlv_time_prop_ds {
+	int16_t current_utc_offset;
+	uint8_t flags;
+	uint8_t time_src;
+} __packed;
+
+/**
+ * @brief TLV data fields representing portDS dataset.
+ */
+struct ptp_tlv_port_ds {
+	struct ptp_port_id id;
+	uint8_t		   state;
+	int8_t		   log_min_delay_req_interval;
+	ptp_timeinterval   mean_link_delay;
+	int8_t		   log_announce_interval;
+	uint8_t		   announce_receipt_timeout;
+	int8_t		   log_sync_interval;
+	uint8_t		   delay_mechanism;
+	int8_t		   log_min_pdelay_req_interval;
+	uint8_t		   version;
+} __packed;
 
 /**
  * @brief Function allocating memory for TLV container structure.
@@ -90,6 +291,15 @@ struct ptp_tlv_container *ptp_tlv_alloc(void);
  * @param[in] tlv_container Pointer to the TLV container structure.
  */
 void ptp_tlv_free(struct ptp_tlv_container *tlv_container);
+
+/**
+ * @brief Function for getting type of action to be taken on recipt of the PTP message.
+ *
+ * @param[in] msg Pointer to the PTP message.
+ *
+ * @return Type of action to be taken.
+ */
+enum ptp_mgmt_op ptp_mgmt_action(struct ptp_msg *msg);
 
 /**
  * @brief Function for getting type of the TLV.
