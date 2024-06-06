0ee8343cffbd64feca57b05fffaa23649468988a
Anas Nashif <anas.nashif@intel.com>
2024-06-06 18:34:22 -0400

    arch: call arch_smp_init() directly, do not use SYS_INIT
    Move this to a call in the init process. arch_* calls are no services
    and should be called consistently during initialization.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- kernel/init.c
+++ kernel/init.c
@@ -661,6 +661,9 @@ FUNC_NORETURN void z_cstart(void)
 	/* perform basic hardware initialization */
 	z_sys_init_run_level(INIT_LEVEL_PRE_KERNEL_1);
 	z_sys_init_run_level(INIT_LEVEL_PRE_KERNEL_2);
+#if defined(CONFIG_SMP)
+	arch_smp_init();
+#endif
 
 #ifdef CONFIG_STACK_CANARIES
 	uintptr_t stack_guard;
